b5743daaad6054a1824d89f9ebc898591bf3d445
mainaerick <niteric@gmainl.com>
2024-11-24 20:21:46 +0300

    chore: conigure https 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,9 +28,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') === 'production') {
-            URL::forceScheme('https');
-        }
+        //
 
     }
 }
