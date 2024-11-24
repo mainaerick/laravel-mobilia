7786318aeae959abd676c8fb90333a6c2f2b78f2
mainaerick <niteric@gmainl.com>
2024-11-24 20:57:05 +0300

    chore: 1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,7 +28,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        dd(config('app.env'));
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
 
     }
 }
