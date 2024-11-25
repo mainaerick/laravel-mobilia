b9b44413fbc73c8237512349212fec2bee5cddfc
mainaerick <niteric@gmainl.com>
2024-11-25 11:21:25 +0300

    chore: config candyfile

---
+++ app/Http/Controllers/CaddyController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CaddyController extends Controller
+{
+    public function check(Request $request)
+    {
+        $authorizedDomains = [
+            'laravel.test',
+            'www.laravel.test',
+            'www.integrictechservices.com',
+            'integrictechservices.com'
+            // Add subdomains here
+        ];
+
+        if (in_array($request->query('domain'), $authorizedDomains)) {
+            return response('Domain Authorized');
+        }
+
+        // Abort if there's no 200 response returned above
+        abort(503);
+    }
+}
